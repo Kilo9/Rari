597fdcb3eee5b2a67c1d2e2dbbf1dde97e5f9ed4
Kilo9 <jbassi@stevens.edu>
2015-01-28 20:09:47 -0500

    add authentication with bcrypt and has_secure_password

--- rari/db/schema.rb
+++ rari/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121173906) do
+ActiveRecord::Schema.define(version: 20150128185328) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "model"
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 20150121173906) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
