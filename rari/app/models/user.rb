dd7c1d8d4deab8e47ad5db136dd4d8fd207ac23f
Kilo9 <jbassi@stevens.edu>
2015-01-30 12:27:14 -0500

    add nav bar styling

--- rari/app/models/user.rb
+++ rari/app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :on => :create
   validates_presence_of :name, :on => :create
   has_secure_password
+
+  #returns the formatted date of when the user joined
+  def birthday
+    return self.created_at.strftime('%b %-d %Y')
+  end
+
 end
